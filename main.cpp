e7f65d722d89cb83389841774b20bbc91ca8f6cf
marina.romashkova <Marina.Romashkova@kaspersky.com>
2022-08-19 22:22:51 +0300

    abstract-vm

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <tclap/CmdLine.h>
 
 #include "Operand.hpp"
 #include "Factory.hpp"
@@ -13,78 +14,31 @@
 #include "abstractVM.h"
 
 
-int main() {
 
-    Factory factory = Factory();
-    IOperand const * factory_test = factory.createOperand(INT32, "123");
-    std::cout << "factory_method " << factory_test->getType() << std::endl;
-    delete factory_test;
+int main(int argc, char **argv) {
 
-    IOperand * test = new Operand<eOperandType>(INT8, "1");
-    IOperand * test1 = new Operand<eOperandType>(INT16, "2");
-    IOperand * test2 = new Operand<eOperandType>(INT32, "3");
-    IOperand * test3 = new Operand<eOperandType>(FLOAT, "4");
-    IOperand * test4 = new Operand<eOperandType>(DOUBLE, "5");
+    const std::string no_file = "no file";
 
-    IOperand const * test_sum = *test1 + *test2;
-    const std::string test_sum_string = test_sum->toString();
-    std::cout << "test sum: " << test_sum_string << std::endl;
-    delete test_sum;
+    TCLAP::CmdLine cmd("Abstract VM", ' ', "1");
+    TCLAP::ValueArg<std::string> fileArg("f","file","File to parse", false,no_file,"string");
+    TCLAP::SwitchArg stdInput("i","input","Use standard input", cmd, false);
 
-    IOperand const * test_dif = *test1 - *test2;
-    const std::string test_dif_string = test_dif->toString();
-    std::cout << "test dif: " << test_dif_string << std::endl;
-    delete test_dif;
+    cmd.add( fileArg);
+    cmd.parse(argc, argv);
 
-    IOperand const * test_product = *test1 * *test2;
-    const std::string test_product_string = test_product->toString();
-    std::cout << "test product: " << test_product_string << std::endl;
-    delete test_product;
+    std::string file = fileArg.getValue();
+    bool useStdInput = stdInput.getValue();
 
-    IOperand const * test_quotient = *test1 / *test2;
-    const std::string test_quotient_string = test_quotient->toString();
-    std::cout << "test quotient: " << test_quotient_string << std::endl;
-    delete test_quotient;
-
-    IOperand const * test_modulo = *test1 % *test2;
-    const std::string test_modulo_string = test_modulo->toString();
-    std::cout << "test modulo: " << test_modulo_string << std::endl;
-    delete test_modulo;
-
-
-    std::vector< IOperand *> stack;
-
-    stack.push_back(test);
-    stack.push_back(test1);
-    stack.push_back(test2);
-    stack.push_back(test3);
-    stack.push_back(test4);
-
-    for (auto it = stack.rbegin(); it != stack.rend(); ++it)
-    {
-        std::cout << (*it)->getType() << std::endl;
+    if (file != no_file) {
+        std::ifstream t(file);
+        std::stringstream buffer;
+        buffer << t.rdbuf();
+    } else if (useStdInput) {
+        //input std here
+    } else {
+        std::cout << "Add -h to know usage" << std::endl;
+        return 0;
     }
-
-    IOperand * testNew = stack.back();
-    std::cout << testNew->getType();
-    stack.pop_back();
-    IOperand * testNew1 = stack.back();
-    std::cout << testNew1->getType();
-    stack.pop_back();
-    IOperand * testNew2 = stack.back();
-    std::cout << testNew2->getType();
-    stack.pop_back();
-    IOperand * testNew3 = stack.back();
-    std::cout << testNew3->getType();
-    stack.pop_back();
-    IOperand * testNew4 = stack.back();
-    std::cout << testNew4->getType() << std::endl;
-    stack.pop_back();
-    delete test;
-    delete test1;
-    delete test2;
-    delete test3;
-    delete test4;
 
 
 // Provide the input text in a stream
